46906fa2f3957029df0773eb96c94a0d12018e34
Duan Li <Duan.Li@vocus.com.au>
2019-01-04 11:00:38 +1100

    update phpcs rules

--- src/SampleOne.php
+++ src/SampleOne.php
@@ -4,31 +4,50 @@ namespace Agilekeys\CodeStandards\Examples;
 
 final class SampleOne
 {
+    /** @var string $name */
     private $name;
 
+    /** @var int $number */
     private $number;
 
+    /**
+     * SampleOne constructor.
+     * @param string $name
+     * @param int $number
+     */
     public function __construct(string $name, int $number)
     {
         $this->name = $name;
         $this->number = $number;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     */
     public function setName(string $name)
     {
         $this->name = $name;
     }
 
+    /**
+     * @return int
+     */
     public function getNumber(): int
     {
         return $this->number;
     }
 
+    /**
+     * @param int $number
+     */
     public function setNumber(int $number)
     {
         $this->number = $number;
